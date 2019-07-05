21980ed8c4f7bda12d6a9c4e1ac6413be9c9595f
Lucas de Paula <lucas.barbosa@microcity.com.br>
2019-07-04 22:36:12 -0300

    v2

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import './img/favicon.ico'
+import './css/index.css'
+
 ReactDOM.render(
     <h1>Desenvolvimento Front-end Avançado</h1>,
     document.querySelector("#main")
